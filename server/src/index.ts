d7d839033cfedbcb3ce838c4d7e1ab8770782c70
hrasamoe <herysamuelpljv@gmail.com>
2026-05-10 21:34:18 +0300

    add auth route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
-// src/index.ts
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
-import { setDefaultResultOrder } from "dns";
-setDefaultResultOrder("ipv4first");
-
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import { initPool } from "./db/pool"; // ← importe initPool pas le pool directement
+import { initPool } from "./db/pool";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+import { setDefaultResultOrder } from "dns";
+import { authRouter } from "./modules/auth/aut.router";
+setDefaultResultOrder("ipv4first");
+
 
 const app = express();
 const PORT = process.env.PORT || 4242;
@@ -21,19 +20,19 @@ app.use(
 );
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+app.use("/api/auth", authRouter);
 
 app.get("/api/health", (_, res) => {
   res.json({ status: "ok", project: "MyScholaria" });
 });
 
-// ← initPool d'abord, puis démarrer le serveur
 initPool()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`🚀 MyScholaria API → http://localhost:${PORT}`);
+      console.log(` MyScholaria API → http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
-    console.error("❌ Erreur démarrage:", err.message);
+    console.error("[!!CRASH] Erreur démarrage:", err.message);
     process.exit(1);
   });
