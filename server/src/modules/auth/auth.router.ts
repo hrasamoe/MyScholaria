fcbaf97a15493b008563bc6bb49d9a1d09e5195d
hrasamoe <herysamuelpljv@gmail.com>
2026-05-11 11:14:10 +0300

    feat(API Route) Verify the token

--- server/src/modules/auth/auth.router.ts
+++ server/src/modules/auth/auth.router.ts
@@ -3,8 +3,10 @@ import { registerSchema, loginSchema } from "./auth.schema";
 import { loginUser, logoutUser, registerUser } from "./auth.service";
 import { RequireAuth, AuthRequest } from "../../middleware/auth.middleware";
 export const authRouter = Router();
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import { ENV } from "../../config/env";
+import { pool } from "../../db/pool";
+import { json } from "zod";
 
 authRouter.post("/register", async (req: Request, res: Response) => {
   try {
@@ -31,16 +33,49 @@ authRouter.post("/login", async (req: Request, res: Response) => {
   }
 });
 
+authRouter.get("/verify-email", async (req: Request, res: Response) => {
+  try {
+    const { token } = req.query;
+    if (!token || typeof token !== "string") {
+      return res.status(400).json({ message: "Missing token" });
+    }
+    const { rows } = await pool.query(
+      `SELECT id FROM users 
+      WHERE verify_token = $1
+      AND verify_expires > NOW()
+      AND is_verified = false`,
+      [token],
+    );
+    if (rows.length === 0) {
+      return (
+        res.status(400),
+        json({
+          message: "Token invalid or expired",
+        })
+      );
+    }
+    const userId = rows[0].id;
+    await pool.query(
+      `UPDATE users
+      SET is_verified = true, verify_token = NULL, verify_expires = NULL
+      WHERE id = $1`[userId],
+    );
+    res.redirect(`${ENV.CLIENT_URL}/auth/signin?verified=true`);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  } finally {
+  }
+});
+
 authRouter.post("/logout", async (req: Request, res: Response) => {
   try {
     const { refreshToken } = req.body;
     if (!refreshToken) {
       return res.status(400).json({ message: "Refresh token required" });
     }
-    const payload = jwt.verify(
-      refreshToken,
-      ENV.JWT_SECRET
-    ) as { userId: string };
+    const payload = jwt.verify(refreshToken, ENV.JWT_SECRET) as {
+      userId: string;
+    };
 
     await logoutUser(payload.userId);
     res.status(200).json({ message: "Logged out successfully" });
