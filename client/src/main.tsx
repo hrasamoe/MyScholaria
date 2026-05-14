e21a6d8bc32bd3caff062ae894115a85aaf525e8
Heritiana <herysamuelpljv@gmail.com>
2026-05-14 17:40:34 +0300

    remove drag interaction et clic droite sur les image

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+document.addEventListener("contextmenu", (e) => {
+  if ((e.target as HTMLElement).tagName === "IMG") {
+    e.preventDefault();
+  }
+});
+
 createRoot(document.getElementById("root")!).render(<App />);
