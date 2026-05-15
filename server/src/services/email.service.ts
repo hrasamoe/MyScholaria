263f94aa316a3cfd0891688fb80f674661704a85
hrasamoe <herysamuelpljv@gmail.com>
2026-05-16 00:42:28 +0300

    change port

--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -3,6 +3,9 @@ import { ENV } from "../config/env";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: ENV.GMAIL_USER,
     pass: ENV.APP_PASSWORD,
