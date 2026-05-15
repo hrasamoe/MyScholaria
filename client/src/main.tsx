325ffdd15ea897981a1cba86e1a044040b769676
hrasamoe <herysamuelpljv@gmail.com>
2026-05-15 21:27:10 +0300

    feat(API Call) Add timeout when we call API

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,4 +8,27 @@ document.addEventListener("contextmenu", (e) => {
   }
 });
 
+const originalFetch = window.fetch;
+
+window.fetch = async function (
+  input: RequestInfo | URL,
+  init?: RequestInit & { timeout?: number },
+) {
+  const timeout = init?.timeout || 10000;
+
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+
+  try {
+    const response = await originalFetch(input, {
+      ...init,
+      signal: controller.signal,
+    });
+    clearTimeout(id);
+    return response;
+  } catch (error) {
+    clearTimeout(id);
+    throw error;
+  }
+};
 createRoot(document.getElementById("root")!).render(<App />);
