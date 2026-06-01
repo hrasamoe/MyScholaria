2c2a47063b7187404dd08d4cd08c807181a65517
hrasamoe <herysamuelpljv@gmail.com>
2026-06-01 11:12:12 +0300

    Adding other form utils

--- server/src/modules/students/students.schema.ts
+++ server/src/modules/students/students.schema.ts
@@ -11,7 +11,10 @@ export const studentSchema = z.object({
   address: z.string().min(5, "Invalid address").optional(),
   gender: z.enum(["male", "female"]),
   classID: z.string().min(1, "Please provide a class"),
+  IDNumber: z.string().min(1, "Please provide an ID number"),
   parentID: z.string().min(1, "Please provide a parent"),
+  status: z.enum(["active", "expelled", "transferred", "graduated"]).default("active"),
+  medical_notes: z.string().optional(),
 });
 
 export type StudentInfo = z.infer<typeof studentSchema>;
