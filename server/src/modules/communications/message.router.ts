609ac861b48f0ec7e80a215dada64e7cf822ab9b
hrasamoe <herysamuelpljv@gmail.com>
2026-06-19 18:32:51 +0300

    Feat (Messages): Add endpoints to delete messages for the user and everyone

--- server/src/modules/communications/message.router.ts
+++ server/src/modules/communications/message.router.ts
@@ -2,29 +2,39 @@ import { Request, Response, Router } from "express";
 import { AuthRequest, RequireAuth } from "../../middleware/auth.middleware";
 import { sendToUser } from "../../services/websocket/ws-server";
 import { MessageSchema } from "./message.schema";
-import { getHistory, getMessages, sendMessage } from "./message.service";
+import {
+  DeleteMessageForEveryone,
+  DeleteMessageForMe,
+  getHistory,
+  getMessages,
+  sendMessage,
+} from "./message.service";
 
 export const messageRouter = Router();
 
-messageRouter.post("/send/:senderID", async (req: Request, res: Response) => {
-  const userID = req.params.senderID as string;
-  const { recipient_id, content } = req.body;
-  try {
-    const parsed = MessageSchema.parse({
-      sender_id: userID,
-      recipient_id,
-      content,
-    });
+messageRouter.post(
+  "/send/:senderID",
+  RequireAuth,
+  async (req: Request, res: Response) => {
+    const userID = req.params.senderID as string;
+    const { recipient_id, content } = req.body;
+    try {
+      const parsed = MessageSchema.parse({
+        sender_id: userID,
+        recipient_id,
+        content,
+      });
 
-    const message = await sendMessage(parsed);
+      const message = await sendMessage(parsed);
 
-    const delivered = sendToUser(recipient_id, message);
-    res.status(200).json({ success: true, message });
-  } catch (error: any) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message, error: error.message });
-  }
-});
+      const delivered = sendToUser(recipient_id, message);
+      res.status(200).json({ success: true, message });
+    } catch (error: any) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message, error: error.message });
+    }
+  },
+);
 
 messageRouter.get(
   "/history-channels",
@@ -55,6 +65,38 @@ messageRouter.get(
       return res
         .status(500)
         .json({ message: error.message, error: error.message });
+    }
+  },
+);
+
+messageRouter.put(
+  "/remove-for-me/:messageID",
+  RequireAuth,
+  async (req: AuthRequest, res: Response) => {
+    const userID = req.userId as string;
+    const messageID = req.params.messageID as string;
+    try {
+      await DeleteMessageForMe(messageID, userID);
+      res.status(200).json({ success: true });
+    } catch (error: any) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message, error: error.message });
+    }
+  },
+);
+
+messageRouter.put(
+  "/remove-for-everyone/:messageID",
+  RequireAuth,
+  async (req: AuthRequest, res: Response) => {
+    const userID = req.userId as string;
+    const messageID = req.params.messageID as string;
+    try {
+      await DeleteMessageForEveryone(messageID, userID);
+      res.status(200).json({ success: true });
+    } catch (error: any) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message, error: error.message });
     }
   },
 );
