b78607966cbe5971601582ebfd9de96f1a4a2bb3
hrasamoe <herysamuelpljv@gmail.com>
2026-06-21 03:26:52 +0300

    Feat (Crypto): Refactor message encryption to use ENV for loading encryption key

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -10,10 +10,11 @@ export const ENV = {
   API_URL: process.env.API_URL,
   RESEND_API_KEY: process.env.RESEND_API_KEY,
   CLIENT_URL: process.env.CLIENT_URL,
-  JWT_SECRET: requireEnv("JWT_SECRET"),
+  // JWT_SECRET: requireEnv("JWT_SECRET"),
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "15m",
   GMAIL_USER: process.env.GMAIL_USER,
   APP_PASSWORD: process.env.GMAIL_APP_PASSWORD,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || "7d",
   PORT: process.env.PORT || "4242",
+  MESSAGE_ENCRYPTION_KEY: process.env.MESSAGE_ENCRYPTION_KEY,
 };
--- server/src/services/crypto/message_encryption.ts
+++ server/src/services/crypto/message_encryption.ts
@@ -1,10 +1,11 @@
 import crypto from "crypto";
+import { ENV } from "../../config/env";
 
 const ALGO = "aes-256-gcm";
 const IV_LENGTH = 12;
 
 function loadMasterKey(): Buffer {
-  const rawKey = process.env.MESSAGE_ENCRYPTION_KEY;
+  const rawKey = ENV.MESSAGE_ENCRYPTION_KEY;
 
   if (!rawKey) {
     throw new Error("MESSAGE ENCRYPTION MISSING");
@@ -32,4 +33,23 @@ export function encryptMessage(message: string): string {
     encrypt_tag.toString("base64"),
     crypted_message.toString("base64"),
   ].join(":");
+}
+
+export function decryptMessage(cryptedMessage: string): string {
+  const value = cryptedMessage.split(":");
+
+  if (value.length !== 3) {
+    throw new Error("Invalid encrypted Message Format");
+  }
+  const iv = Buffer.from(value[0], "base64");
+  const authTag = Buffer.from(value[1], "base64");
+  const crypted = Buffer.from(value[2], "base64");
+
+  const decrypt = crypto.createDecipheriv(ALGO, ENCRYPTION_KEY, iv);
+  decrypt.setAuthTag(authTag);
+  const cleanMessage: any = Buffer.concat([
+    decrypt.update(crypted),
+    decrypt.final(),
+  ]);
+  return cleanMessage.toString("utf8");
 }
