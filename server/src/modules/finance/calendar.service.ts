867ab344fa836bab09df4c8aed3e21f12947948f
hrasamoe <herysamuelpljv@gmail.com>
2026-06-22 10:03:12 +0300

    feat(calendar): implement createCalendar function for adding events to the school calendar

---
+++ server/src/modules/finance/calendar.service.ts
@@ -0,0 +1,91 @@
+import { pool } from "../../db/pool";
+import { EventInfo } from "./calendar.schema";
+
+export async function createCalendar(userID: string, Event: EventInfo) {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+
+    const establishment = await client.query(
+      "SELECT establishment_id FROM establishment_members WHERE user_id = $1",
+      [userID],
+    );
+
+    const establishmentID = establishment.rows[0]?.establishment_id;
+    if (!establishmentID) {
+      throw new Error("User establishment not found");
+    }
+
+    let result;
+
+    if (Event.isMultiDay) {
+      const queryText = `
+        INSERT INTO school_calendar (
+          title,
+          event_type,
+          is_multiple_day,
+          description,
+          author_id,
+          establishment_id,
+          start_date,
+          end_date,
+          start_time,
+          end_time
+        )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+        RETURNING *
+      `;
+      const values = [
+        Event.title,
+        Event.type,
+        Event.isMultiDay,
+        Event.description || null,
+        userID,
+        establishmentID,
+        Event.startDate,
+        Event.endDate,
+        Event.startTime,
+        Event.endTime,
+      ];
+      result = await client.query(queryText, values);
+    } else {
+      const queryText = `
+        INSERT INTO school_calendar (
+          title,
+          event_type,
+          is_multiple_day,
+          description,
+          author_id,
+          establishment_id,
+          date,
+          start_time,
+          end_time
+        )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+        RETURNING *
+      `;
+      const values = [
+        Event.title,
+        Event.type,
+        Event.isMultiDay,
+        Event.description || null,
+        userID,
+        establishmentID,
+        Event.date,
+        Event.startTime,
+        Event.endTime,
+      ];
+      result = await client.query(queryText, values);
+    }
+
+    await client.query("COMMIT");
+    return result.rows[0];
+  } catch (error: any) {
+    console.error(error);
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
