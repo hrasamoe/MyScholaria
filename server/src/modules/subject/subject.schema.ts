5da084343c6eda673e915123c92ed689a5623cda
hrasamoe <herysamuelpljv@gmail.com>
2026-06-22 17:27:42 +0300

    feat(subject): add subject schema validation using Zod

---
+++ server/src/modules/subject/subject.schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const SubjectSchema = z.object({
+  code: z.string().min(3, "Please create a subject Code at least 3 charachter"),
+  name: z.string().min(3, "Please provide a valid subject name"),
+  level: z.string().min(3, "Class required"),
+  coefficient: z.number().int().nonnegative(),
+  hour_per_week: z.number().int().nonnegative(),
+});
+
+export type SubjectInfo = z.infer<typeof SubjectSchema>
