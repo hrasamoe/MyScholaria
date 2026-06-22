64692f5070c5764d9af53173ceeb614f528ba167
hrasamoe <herysamuelpljv@gmail.com>
2026-06-22 10:57:34 +0300

    feat(calendar): implement validation and creation logic in createCalendar endpoint

--- server/src/modules/finance/calendar.router.ts
+++ server/src/modules/finance/calendar.router.ts
@@ -1,18 +1,30 @@
 import { Response, Router } from "express";
 import { AuthRequest, RequireAuth } from "../../middleware/auth.middleware";
-
+import { calendarSchema } from "./calendar.schema";
+import { createCalendar } from "./calendar.service";
 export const calendarRouter = Router();
 
 calendarRouter.post(
   "/create",
   RequireAuth,
   async (req: AuthRequest, res: Response) => {
+    const userID = req.userId as string;
     try {
+      const parsed = calendarSchema.safeParse(req.body);
+      if (!parsed.success) {
+        return res
+          .status(400)
+          .json({ message: "Invalid data", errors: parsed.error });
+      }
+
+      const result = await createCalendar(userID, parsed.data);
+      res.status(201).json(result);
     } catch (error: any) {
       res.status(500).json({ message: error.message });
     }
   },
 );
+
 calendarRouter.post(
   "/delete/:id",
   RequireAuth,
