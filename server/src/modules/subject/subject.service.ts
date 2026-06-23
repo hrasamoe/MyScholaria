e6400375aa06071d703bb14d618c75559d340c16
hrasamoe <herysamuelpljv@gmail.com>
2026-06-23 03:48:17 +0300

    feat(subject): add createSubject function to insert new subjects into the database

--- server/src/modules/subject/subject.service.ts
+++ server/src/modules/subject/subject.service.ts
@@ -5,7 +5,29 @@ export async function createSubject(userID: string, subjectData: SubjectInfo) {
   const client = await pool.connect();
   try {
     await client.query("BEGIN");
+    const queryText = `INSERT INTO subject 
+    (
+      code,
+      name,
+      level,
+      coefficient,
+      hours_per_week,
+      created_at,
+      created_by
+    ) VALUES ($1,$2,$3,$4,$5,$6,$7)
+      `;
+    const value = [
+      subjectData.code,
+      subjectData.name,
+      subjectData.level,
+      subjectData.coefficient,
+      subjectData.hours_per_week,
+      new Date(),
+      userID,
+    ];
+    const result = await client.query(queryText, value);
     await client.query("COMMIT");
+    return result.rows[0];
   } catch (error: any) {
     console.error(error);
     await client.query("ROLLBACK");
