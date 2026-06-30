cc2b8a2871d1758a470da4b6ffc4b0fb6c684031
hrasamoe <herysamuelpljv@gmail.com>
2026-06-30 22:07:11 +0300

    feat(tuition): implement tuition management routes and fix createTuition function

---
+++ server/src/modules/finance/tuition.router.ts
@@ -0,0 +1,77 @@
+import { Router, Response, Request } from "express";
+import {
+  AuthRequest,
+  RequireAuth,
+  RequireAuthOnly,
+} from "../../middleware/auth.middleware";
+import { createTuition, editTuition, getTuitionList } from "./tuition.service";
+import { TuitionSchema } from "./tuition.schema";
+export const financeRouter = Router();
+
+financeRouter.post(
+  "/tuition-rules",
+  RequireAuth,
+  async (req: AuthRequest, res: Response) => {
+    try {
+      const userID = req.userId as string;
+      const parsed = TuitionSchema.safeParse(req.body);
+      if (!parsed.success) {
+        return res.status(400).json({ error: parsed.error.format() });
+      }
+      const newTuition = await createTuition(parsed.data, userID);
+      res
+        .status(201)
+        .json({ message: "Tuition created with success", data: newTuition });
+    } catch (error: any) {
+      res.status(500).json({ message: error.message, error: error.message });
+    }
+  },
+);
+
+financeRouter.get(
+  "/tuition-rules",
+  RequireAuth,
+  async (req: AuthRequest, res: Response) => {
+    try {
+      const establishmentID = req.establishmentID as string;
+      if (!establishmentID) {
+        return res
+          .status(400)
+          .json({ message: "Missing establishmentID query parameter" });
+      }
+
+      const tuitionList = await getTuitionList(establishmentID);
+      res.status(200).json(tuitionList);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message, error: error.message });
+    }
+  },
+);
+
+financeRouter.put(
+  "/tuition-rules/:id",
+  RequireAuth,
+  async (req: AuthRequest, res: Response) => {
+    const tuitionID = req.params.id as string;
+    const parsed = TuitionSchema.safeParse(req.body);
+    const userID = req.userId as string;
+    if (!parsed.success) {
+      return res.status(400).json({ error: parsed.error.format() });
+    }
+    if (!tuitionID) {
+      throw new Error("Missing tuitionID");
+    }
+    try {
+      const result = await editTuition(tuitionID, parsed.data, userID);
+      res.status(201).json({
+        message: "Tuition updated successfully",
+        data: result,
+      });
+    } catch (error: any) {
+      res.status(500).json({
+        message: error.message,
+        error: error.message,
+      });
+    }
+  },
+);
--- server/src/modules/finance/tuition.service.ts
+++ server/src/modules/finance/tuition.service.ts
@@ -1,21 +1,22 @@
 import { pool } from "../../db/pool";
 import { TuitionInfo } from "./tuition.schema";
 
-export async function creteTuition(TuitionData: TuitionInfo, userID: string) {
+export async function createTuition(TuitionData: TuitionInfo, userID: string) {
   const client = await pool.connect();
   try {
     await client.query("BEGIN");
+
     const insertQuery = `INSERT INTO fee_configurations (
-    class_id,
-    tuition_fee,
-    late_fee,
-    registration_fee,
-    academic_year,
-    establishment_id,
-    created_by,
-    created_at,
-    updated_at
-  ) VALUES ($1, $2, $3, $4, $5, $6, $7, NOW(), NOW()) RETURNING *`;
+      class_id,
+      tuition_fee,
+      registration_fee,
+      academic_year,
+      establishment_id,
+      created_by,
+      created_at,
+      updated_at
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+    const now = new Date().toISOString();
     const values = [
       TuitionData.classID,
       TuitionData.tuitionFee,
@@ -23,11 +24,105 @@ export async function creteTuition(TuitionData: TuitionInfo, userID: string) {
       TuitionData.academicYear,
       TuitionData.establishmentID,
       userID,
+      now,
+      now,
     ];
+
     const result = await client.query(insertQuery, values);
 
     await client.query("COMMIT");
-    return result;
+    return result.rows[0];
+  } catch (error: any) {
+    console.log(error);
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
+export async function getTuitionList(establishentID: string) {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    const result = await client.query(
+      `SELECT
+    fc.id,
+    fc.class_id,
+    r.name AS room_name,
+    c.name AS class_name,
+    fc.tuition_fee,
+    fc.registration_fee,
+    CONCAT_WS(' ', p.first_name, p.last_name) AS teacher_full_name,
+    fc.academic_year
+  FROM fee_configurations fc
+  JOIN classes c ON fc.class_id = c.id
+  JOIN teachers t ON c.main_teacher_id = t.id
+  JOIN profiles p ON t.profile_id = p.id
+  JOIN rooms r ON c.room_id = r.id
+  WHERE fc.establishment_id = $1`,
+      [establishentID],
+    );
+    await client.query("COMMIT");
+    return result.rows;
+  } catch (error: any) {
+    console.log(error);
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
+export async function editTuition(
+  TuitionID: string,
+  TuitionData: TuitionInfo,
+  userID: string,
+) {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    const rowsToUpdate = await client.query(
+      "SELECT * FROM fee_configurations WHERE id = $1",
+      [TuitionID],
+    );
+    if (rowsToUpdate.rowCount === 0) {
+      throw new Error("Tuition not found");
+    }
+    if (rowsToUpdate.rows[0].created_by !== userID) {
+      throw new Error("You are not allowed to modify this Tuition");
+    }
+    const updateQuery = `UPDATE fee_configurations SET
+        tuition_fee = $1, 
+        registration_fee = $2,
+        academic_year = $3,
+        updated_at = $4
+      WHERE id = $5 RETURNING *`;
+    const now = new Date().toISOString();
+    const values = [
+      TuitionData.tuitionFee,
+      TuitionData.registrationFee,
+      TuitionData.academicYear,
+      now,
+      TuitionID,
+    ];
+    const result = await client.query(updateQuery, values);
+    await client.query("COMMIT");
+    return result.rows[0];
+  } catch (error: any) {
+    console.log(error);
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+}
+
+export async function deleteTuition(TuitionID: string, userID: string) {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    await client.query("COMMIT");
   } catch (error: any) {
     console.log(error);
     await client.query("ROLLBACK");
